8764fff36e3f258a422169d66f6bc99b0a7071ad
sawaizmirza <m_sawaiz1791@ymail.com>
2023-12-21 15:57:43 +0000

    project 2

---
+++ problem2.py
@@ -0,0 +1,13 @@
+# https://projecteuler.net/problem=2
+
+def sum_fibonacci_even_values(limit):
+    a, b = 0, 1
+    total = 0
+    while a < limit:
+        if a % 2 == 0:
+            total += a
+        a, b = b, a + b
+    return total    
+
+result = sum_fibonacci_even_values(4000000)
+print(result)
